6a52d01ad84ade2c41289bbae10915fddd330bb3
Cihangir SAVAS <cihangir@koding.com>
2016-04-10 00:12:56 -0700

    elb: add more output values

--- elb/elb.tf
+++ elb/elb.tf
@@ -54,3 +54,27 @@ resource "aws_elb" "elb" {
     project = "${var.name}"
   }
 }
+
+output "aws_elb_elb_id" {
+  value = "${aws_elb.elb.id}"
+}
+
+output "aws_elb_elb_name" {
+  value = "${aws_elb.elb.name}"
+}
+
+output "aws_elb_elb_dns_name" {
+  value = "${aws_elb.elb.dns_name}"
+}
+
+output "aws_elb_elb_instances" {
+  value = "${aws_elb.elb.instances}"
+}
+
+output "aws_elb_elb_source_security_group_id" {
+  value = "${aws_elb.elb.source_security_group_id}"
+}
+
+output "aws_elb_elb_zone_id" {
+  value = "${aws_elb.elb.elb_zone_id}"
+}
